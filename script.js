9bbbd13767d764d6706a6a17d1c3fafebfb83609
DungeonCR <dungeoncr@icloud.com>
2021-07-12 22:22:00 +0800

    better

--- script.js
+++
@@ -1,6 +0,0 @@
-let btnOpen = document.querySelector('button');
-let input = document.querySelector('input');
-
-btnOpen.addEventListener('click', () => {
-    window.open('https://nhentai.to/g/' + input.value, '_black');
-});
